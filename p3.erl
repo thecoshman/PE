b9e4ea0334b7279ceb24cbd8b01e7c10f6b76ce7
thecoshman <thecoshman@gmail.com>
2015-06-28 17:16:57 +0100

    Update p3.erl
    Fixed up bug in logic for 'isPrime' and added final logic to actually provide the answer.

--- p3.erl
+++ p3.erl
@@ -1,37 +1,33 @@
 -module(p3).
 -import(math,[sqrt/1]).
+-import(lists, [last/1]).
 -export([solve/0, isPrime/1, primeFactors/1, nextPrimeAfter/1]).
 
 solve() ->
-    solve(13195).
+    solve(600851475143).
 
 solve(N) ->
-    primeFactors(N).
+    last(primeFactors(N)).
 
 primeFactors(N) ->
     case isPrime(N) of
-        true -> N;
+        true -> [N];
         false -> 
             {L, R} = findPrimeFactor(N),
-            [primeFactors(L), primeFactors(R)]
+            primeFactors(L) ++ primeFactors(R)
     end.
-%primeFactors(N) -> [N].
 
-isPrime(1) -> false;
-isPrime(2) -> true;
-isPrime(3) -> true;
-isPrime(5) -> true;
-isPrime(N) when N rem 2 == 0 -> false;
-isPrime(N) when N rem 3 == 0 -> false;
+isPrime(N) when N =< 3 -> N > 1;
+isPrime(N) when N rem 2 == 0 ; N rem 3 == 0 -> false;
 isPrime(N) -> isPrime(N, 5, ceiling(sqrt(N))).
 
-isPrime(N, T, _) when N rem T == 0 -> false;
 isPrime(_, T, Max) when T > Max -> true;
+isPrime(N, T, _) when N rem T == 0 ; N rem (T + 2) == 0 -> false;
 isPrime(N, T, Max) -> isPrime(N, T+6, Max).
 
 findPrimeFactor(N) -> findPrimeFactor(N, 2).
 
-findPrimeFactor(N, F) when N rem F == 0 -> {F, N / F};
+findPrimeFactor(N, F) when N rem F == 0 -> {F, N div F};
 findPrimeFactor(N, F) ->
     Next = nextPrimeAfter(F),
     findPrimeFactor(N, Next).
@@ -42,8 +38,6 @@ nextPrimeAfter(N) ->
         false -> nextPrimeAfter(N + 1)
     end.
 
-
-
 ceiling(X) when X < 0 ->
     trunc(X);
 ceiling(X) ->
@@ -52,4 +46,3 @@ ceiling(X) ->
         true -> T;
         false -> T + 1
     end.
-
